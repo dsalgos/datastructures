f018718f89f4348aa7248cfb8c4b79874dedd340
ds algos <ds.algos143@gmail.com>
2018-07-24 11:35:18 +0530

    Commiting changes

--- SLinkedList/src/com/dsalgos/datastructures/linkedlist/impl/SinglyLinkedList.java
+++ SLinkedList/src/com/dsalgos/datastructures/linkedlist/impl/SinglyLinkedList.java
@@ -1,6 +1,7 @@
 package com.dsalgos.datastructures.linkedlist.impl;
 
 import com.dsalgos.datastructures.linkedlist.list.List;
+import com.dsalgos.datastructures.linkedlist.node.Node;
 
 /**
  * 
@@ -11,11 +12,27 @@ import com.dsalgos.datastructures.linkedlist.list.List;
  */
 public class SinglyLinkedList<T> implements List<T>{
 
+	private Node<T> head;
+	private Node<T> tail;
+	
+	
 	@Override
 	/**
 	 * 
 	 */
 	public void add(T data) {
+		
+		Node<T> node = new Node<>(data);
+		if(head == null) {
+			head = node;
+			tail = node;
+		} else if(head.getNext() != null) {
+			tail.setNext(node);
+			tail = node;
+		}		
+	}
+	
+	public void add(T data, int position) {
 		
 	}
 
